36c5020bdafbdcfcc559abcf8646e2e4b5ae55a0
tuyc17 <t2017013579@163.com>
2019-12-18 14:45:45 +0800

    trial-2

--- BackEnd_dev/test_app/views.py
+++ BackEnd_dev/test_app/views.py
@@ -502,7 +502,36 @@ def purchaseTicket(request):
         ticket.is_valid = True
 
         # create a QRCode
-        getlogoQRCode(ticket.ticket_id) 
+        # getlogoQRCode(ticket.ticket_id)
+        qr = qrcode.QRCode(
+            version=5,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=8,
+            border=4,
+        )
+        url = str(ticket.ticket_id)
+        qr.add_data(url)
+        qr.make(fit=True)
+        img = qr.make_image()
+        img = img.convert("RGBA")
+        icon = Image.open('media/logo.png')
+        img_w, img_h = img.size
+        factor = 4
+        size_w = int(img_w / factor)
+        size_h = int(img_h / factor)
+        icon_w, icon_h = icon.size
+        if icon_w >size_w:
+            icon_w = size_w
+        if icon_h > size_h:
+            icon_h = size_h
+        icon = icon.resize((icon_w,icon_h),Image.ANTIALIAS)
+        w = int((img_w - icon_w) / 2)
+        h = int((img_h - icon_h) / 2)
+        icon = icon.convert("RGBA")
+        img.paste(icon, (w, h), icon)
+
+        img.save('media/QR/' + str(id) +'.png')
+        test_ticket.QRCode = 'QR/' + str(id) +'.png'
 
         # save ticket
         ticket.save()
