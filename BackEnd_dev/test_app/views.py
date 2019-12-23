7e1542e138c1bb5d9cee02937db2c25a8e654e50
tuyc17 <t2017013579@163.com>
2019-12-24 02:12:36 +0800

    fix test

--- BackEnd_dev/test_app/views.py
+++ BackEnd_dev/test_app/views.py
@@ -944,15 +944,16 @@ def checkTicket(request):
             }
             return JsonResponse(ret)
 
-    except:
-        # 由于try中已包含几乎所有情况，出现的except按照查无此票处理
-        # ret msg
-        ret = {'code': '324', 'msg': None, 'data':{}}
-        ret['msg'] = '检票失败，该票不存在'
-        ret['data'] = {
-            'ticket_id': ticket_id
-        }
-        return JsonResponse(ret)
+    except Exception as e:
+        # # 由于try中已包含几乎所有情况，出现的except按照查无此票处理
+        # # ret msg
+        # ret = {'code': '324', 'msg': None, 'data':{}}
+        # ret['msg'] = '检票失败，该票不存在'
+        # ret['data'] = {
+        #     'ticket_id': ticket_id
+        # }
+        # return JsonResponse(ret)
+        return JsonResponse(e)
 
 '''
 Part 3
